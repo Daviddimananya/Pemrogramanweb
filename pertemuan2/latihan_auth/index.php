051443c61dc26f6ebd90b4c9593b69b6d844c736
Daviddimananya <daviddejavuragna@gmail.com>
2022-09-30 11:05:36 +0700

    semua

---
+++ pertemuan2/latihan_auth/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$database = [
+    'username' => 'Radhea',
+    'password' => '12345'
+];
+
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username == $database['username'] && $password == $database['password']){
+        $_SESSION['username'] = $username;
+
+        header("location:admin.php");
+    }else{
+        echo '<script>alert("Username atau Password salah!");
+        window.location="login-page.php; </script>';
+    }
+}else{
+    echo '<script> window.location="login-page.php";</script>';
+}
+?>
